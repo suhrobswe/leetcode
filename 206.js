3f80c78945c3f41aecf7b3abf32de52b018436e5
suhrobswe <suhrobabdurazzok@gmail.com>
2026-06-15 10:08:03 +0500

    206

---
+++ 206.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let node = null;
+
+  while (head) {
+    const temp = head.next;
+    head.next = node;
+    node = head;
+    head = temp;
+  }
+
+  return node;
+};
